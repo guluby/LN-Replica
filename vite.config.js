ffa5d923a80ae5b218d3b1f5ad49f5bb987b9499
Katsura <45928704+Katsurav@users.noreply.github.com>
2025-05-13 14:48:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
       server: {
         host: "0.0.0.0",    
         port: 8080,
-        allowedHosts: ['ln-replica-151472627439.us-central1.run.app'],
+        allowedHosts: ['livenation-front-end.ashymeadow-4ce473cb.westus2.azurecontainerapps.io'],
       },
       preview: {
         port: 4173
